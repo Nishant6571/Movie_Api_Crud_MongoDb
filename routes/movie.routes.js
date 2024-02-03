0a1f5938a45ab06f0c4b4a71f8323a448b0e01e9
Nishant6571 <nishantbhargava.bhargava@gmail.com>
2024-02-03 18:07:18 +0530

    update

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -11,7 +11,56 @@ movieRouter.get("/movies", async (req, res) => {
     res.status(200).send({ msg: "List of Movies", movies });
   } catch (err) {
     console.error("Error:", err);
-    res.status(500).send({ msg: "Internal Server Error" }, movies);
+    res.status(500).send({ msg: "Internal Server Error" });
+  }
+});
+
+// Get request for filtering and sorting
+movieRouter.get("/movies/query", async (req, res) => {
+  try {
+    const query = {};
+
+    if (req.query.title) {
+      query.title = new RegExp(req.query.title, "i");
+    }
+
+    if (req.query.rating) {
+      query.rating = req.query.rating;
+    }
+
+    if (req.query.releaseYear) {
+      query.releaseYear = req.query.releaseYear;
+    }
+
+    let sort = "name";
+    if (req.query.sortBy) {
+      const sortOrder = req.query.sortBy.endsWith("_desc") ? -1 : 1;
+      const sortField = req.query.sortBy
+        .replace(/_desc$/, "")
+        .replace(/_asc$/, "");
+
+      sort = { [sortField]: sortOrder };
+    }
+
+    let page = 1;
+    let limit = 5;
+
+    if (req.query.page) {
+      page = +req.query.page;
+    }
+    if (req.query.limit) {
+      limit = +req.query.limit;
+    }
+
+    const movies = await MovieModel.find(query)
+      .sort(sort)
+      .skip((page - 1) * limit)
+      .limit(limit);
+
+    res.status(200).send({ msg: "List of Movies", movies });
+  } catch (err) {
+    console.error("Error:", err);
+    res.status(500).send({ msg: "Internal Server Error" });
   }
 });
 
